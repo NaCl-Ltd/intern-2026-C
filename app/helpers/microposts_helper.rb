116f424877699d4ea70654b90be7105a0cd1979d
naganaga0203 <shinta.naga@icloud.com>
2026-08-27 11:32:01 +0900

    #の色を水色にする

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,7 @@
 module MicropostsHelper
+  def highlight_hashtags(content)
+    content.to_s.gsub(/(#[^\s#]+)/) do |hashtag|
+      content_tag(:span, hashtag, class: "hashtag")
+    end.html_safe
+  end
 end
